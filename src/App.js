231ce85c8c3adca24affc03419185950b19aae3a
Michael.Figueroa <michael.figueroa@figstudios.io>
2021-03-20 19:22:20 -0400

    update Landing page for demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+const API_URL = 'https://xbie8fzpf1.execute-api.us-west-1.amazonaws.com/dev';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    const url = `${API_URL}/customers/`;
+    axios.get(url).then(response => response.data)
+    .then((data) => {
+      this.setState({ users: data })
+      console.log(this.state.users)
+     })
+  }
+
+  render() {
+
+    return (
+       <div className="container">
+        <div className="col-xs-8">
+        <h1>React Axios Example</h1>
+        {this.state.users.map((user) => (
+          <div key={user.id} className="card">
+           <div className="card-body">
+               <h5 className="card-title">{user.name}</h5>
+            </div>
+          </div>
+        ))}
+        </div>
+       </div>
+    );
+  }
+  // [...]
 }
-
 export default App;
